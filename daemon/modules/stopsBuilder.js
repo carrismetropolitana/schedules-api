4b1acd56e4569c2451688c9cb113839e76faa5be
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-04-12 17:22:43 +0100

    Testing ChatGPT query

--- daemon/modules/stopsBuilder.js
+++ daemon/modules/stopsBuilder.js
@@ -15,12 +15,12 @@ async function getUniqueRouteShortNamesAtEachStop() {
   const [rows, fields] = await GTFSParseDB.connection.execute(
     `
         SELECT 
+            stops.stop_id, 
             stops.stop_name, 
             stops.stop_lat, 
             stops.stop_lon, 
             routes.route_short_name,
             trips.trip_headsign,
-            stop_times.arrival_time, 
             stop_times.departure_time 
         FROM 
             stops 
@@ -28,8 +28,8 @@ async function getUniqueRouteShortNamesAtEachStop() {
             JOIN trips ON stop_times.trip_id = trips.trip_id 
             JOIN routes ON trips.route_id = routes.route_id 
         ORDER BY 
-            stops.stop_name, 
-            stop_times.arrival_time;
+            stops.stop_id, 
+            stop_times.departure_time;
     `
   );
   const elapsedTime = timeCalc.getElapsedTime(startTime);
@@ -53,9 +53,36 @@ module.exports = {
     // Get all stops from GTFS table (stops.txt)
     const allStopsWithRoutes = await getUniqueRouteShortNamesAtEachStop();
 
+    const stopData = {};
+
+    allStopsWithRoutes.forEach((row) => {
+      if (!(row.stop_id in stopData)) {
+        stopData[row.stop_id] = {
+          stop_id: row.stop_id,
+          stop_name: row.stop_name,
+          stop_lat: row.stop_lat,
+          stop_lon: row.stop_lon,
+          touches: [],
+        };
+      }
+      stopData[row.stop_id].touches.push({
+        route_short_name: row.route_short_name,
+        trip_headsign: row.trip_headsign,
+        departure_time: row.departure_time,
+      });
+    });
+
+    const output = Object.values(stopData);
+
+    console.log(output[0]);
+    console.log(output[1]);
+    console.log(output[2]);
+    console.log(output[3]);
+
+    return;
+
     for (const currentStop of allStopsWithRoutes) {
       console.log(currentStop);
-      return;
       //
       // Record the start time to later calculate duration
       const startTime = process.hrtime();
