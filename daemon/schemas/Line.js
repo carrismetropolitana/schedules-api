010015deeb699ba5ced054a3f2d150ecd020b236
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-04-18 15:08:30 +0100

    Updated schema

--- daemon/schemas/Line.js
+++ daemon/schemas/Line.js
@@ -43,8 +43,69 @@ module.exports = new mongoose.Schema(
     ],
     patterns: [
       {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Pattern',
+        pattern_id: {
+          type: String,
+          maxlength: 100,
+        },
+        direction_id: {
+          type: String,
+          maxlength: 100,
+        },
+        headsign: {
+          type: String,
+          maxlength: 100,
+        },
+        trips: [
+          {
+            trip_id: {
+              type: String,
+              maxlength: 100,
+              unique: true,
+            },
+            shape_id: {
+              type: String,
+              maxlength: 100,
+            },
+            calendar_desc: {
+              type: String,
+              maxlength: 100,
+            },
+            dates: [
+              {
+                type: String,
+                maxlength: 8,
+              },
+            ],
+            schedule: [
+              {
+                stop_id: {
+                  type: String,
+                  maxlength: 6,
+                },
+                stop_sequence: {
+                  type: String,
+                  maxlength: 5,
+                },
+                arrival_time: {
+                  type: String,
+                  maxlength: 8,
+                },
+                arrival_time_operation: {
+                  type: String,
+                  maxlength: 8,
+                },
+                departure_time: {
+                  type: String,
+                  maxlength: 8,
+                },
+                departure_time_operation: {
+                  type: String,
+                  maxlength: 8,
+                },
+              },
+            ],
+          },
+        ],
       },
     ],
   },
