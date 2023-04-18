698f32b9ad9a11e08ac564dff12e7163fdeded25
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-04-18 15:58:42 +0100

    Deregistered RouteSummary schema

--- daemon/databases/gtfsapidb.js
+++ daemon/databases/gtfsapidb.js
@@ -6,7 +6,6 @@ const { GTFSAPIDB_USER, GTFSAPIDB_PASSWORD, GTFSAPIDB_HOST, GTFSAPIDB_NAME } = p
 class GTFSAPIDB {
   constructor() {
     this.connection = mongoose.createConnection(`mongodb://${GTFSAPIDB_USER}:${GTFSAPIDB_PASSWORD}@${GTFSAPIDB_HOST}/${GTFSAPIDB_NAME}?authSource=admin`);
-    this.RouteSummary = this.connection.model('RouteSummary', require('../schemas/RouteSummary'));
     this.Line = this.connection.model('Line', require('../schemas/Line'));
     this.Shape = this.connection.model('Shape', require('../schemas/Shape'));
     this.Stop = this.connection.model('Stop', require('../schemas/Stop'));
